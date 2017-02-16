e9774461b07aea6a28f5883e2aa6b7fd64dfbbf2
Jackson Weidmann <Jackson.weidmann@gmail.com>
2017-02-15 21:39:29 -0800

    Update ant.js

--- ant.js
+++ ant.js
@@ -5,12 +5,15 @@ var lastax = ax;
 var lastay = ay;
 var scale = 10;
 var dir=0;
+var speed;
+var inter;
 //I'm not sure how to make this faster.
 window.onload=function(){
   c=document.getElementById("canvas");
   cc=c.getContext('2d');
   init();
-  setInterval(update,.0001);
+  speed = document.getElementById("speedBar").value;
+  inter = setInterval(update,1000/speed);
 }
 function init() {
   for (var x = 0; x < c.width/scale; x++) {
@@ -40,14 +43,19 @@ function move (thang) {
   }else if (dir==-1) {
     dir=3;
   }
-  if(dir==0){
-    ay-=1;
-  } else if (dir==1) {
-    ax+=1
-  } else if (dir==2) {
-    ay+=1
-  } else if (dir==3) {
-    ax-=1;
+  switch (dir) {
+    case 0:
+      ay-=1;
+      break;
+    case 1:
+      ax+=1;
+      break;
+    case 2:
+      ay+=1;
+      break;
+    case 3:
+      ax-=1;
+      break;
   }
   swap();
 }
@@ -68,4 +76,19 @@ function draw() {
   cc.fillRect(lastax*scale,lastay*scale,scale,scale);
   cc.fillStyle="#DF2A2A";
   cc.fillRect((ax+.5)*scale,(ay+.5)*scale,scale/2,scale/2);
+}
+function redoGrid() {
+  for (var x = 0; x < grid.length; x++) {
+    for (var y = 0; y < grid[x].length; y++) {
+      grid[x][y]=false;
+    }
+  }
+  cc.fillStyle="#947422";
+  cc.fillRect(0,0,c.width,c.height);
+  ax = 36;
+  ay = 32;
+  lastax = ax;
+  lastay = ay;
+  dir=0;
+  console.log("Reset");
 }
